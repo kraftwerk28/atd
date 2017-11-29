19232676148463ae11ba15aaf05dbbe23d443e11
kraftwerk28 <kefirchik3@gmail.com>
2017-11-29 22:11:37 +0200

    added audio to buttons

---
+++ Assets/Scripts/GUI/ButtonSound.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour {
+	public void Press()
+	{
+		GetComponent<AudioSource>().Play();
+	}
+}
